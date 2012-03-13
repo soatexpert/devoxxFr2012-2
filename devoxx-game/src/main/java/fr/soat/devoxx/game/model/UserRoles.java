588d34a6b8b8c663be58d030f1367f6f3a6d168c
aurelien <aure77+github@gmail.com>
2012-03-13 14:04:41 +0100

    Equals + HashCode

--- devoxx-game/src/main/java/fr/soat/devoxx/game/model/UserRoles.java
+++ devoxx-game/src/main/java/fr/soat/devoxx/game/model/UserRoles.java
@@ -67,6 +67,31 @@ public class UserRoles {
 
     public void setRoleName(String roleName) {
         this.roleName = roleName;
+    }    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((roleName == null) ? 0 : roleName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserRoles other = (UserRoles) obj;
+        if (roleName == null) {
+            if (other.roleName != null)
+                return false;
+        } else if (!roleName.equals(other.roleName))
+            return false;
+        return true;
     }
 
     @Override
