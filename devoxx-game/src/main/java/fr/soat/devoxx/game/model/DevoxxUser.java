7aa9c679c65a2d397c86c27f479210790ff2e0c9
soatexpert <experts@soat.fr>
2012-03-26 22:56:20 +0200

    correction TU

---
+++ devoxx-game/src/main/java/fr/soat/devoxx/game/model/DevoxxUser.java
@@ -0,0 +1,150 @@
+package fr.soat.devoxx.game.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "DEVOXX_USER")
+public class DevoxxUser implements Serializable, UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long userId;
+
+    @Column(name = "NAME", unique = true, nullable = false)
+    String userName;
+
+    @Column(name = "FORNAME")
+    String userForname;
+
+    @Column(name = "EMAIL")
+    String userEmail;
+    
+    @Column(name = "REGEMENT_ACCEPTED")
+    boolean reglementAccepted;
+
+    @Column(name = "COMMERCIAL_ACCEPTED")
+    boolean commercialEmailAccepted;
+
+    @Column(name = "NEWS_ACCEPTED")
+    boolean nextEventsAccepted;
+    
+    @Column(name = "QRCODE_ACCPETED")
+    boolean isAcceptedQrCode;
+    
+    
+    
+    @OneToMany
+    List<BundleUserQuestions> bundleUserQuestions;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "USER_USER_ROLES", joinColumns = @JoinColumn(name = "ID_USER"), inverseJoinColumns = @JoinColumn(name = "ID_ROLE"))
+    List<UserRoles> userRoles = new ArrayList<UserRoles>();
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserForname() {
+        return userForname;
+    }
+
+    public void setUserForname(String userForname) {
+        this.userForname = userForname;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public String getPassword() {
+        return userName;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    public boolean isEnabled() {
+        return true;
+    }    
+
+    public List<BundleUserQuestions> getBundleUserQuestions() {
+        return bundleUserQuestions;
+    }
+
+    public void setBundleUserQuestions(List<BundleUserQuestions> bundleUserQuestions) {
+        this.bundleUserQuestions = bundleUserQuestions;
+    }
+
+    public List<UserRoles> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRoles> userRoles) {
+        this.userRoles = userRoles;
+    }
+    
+    public void addUserRole(UserRoles userRole) {
+        this.userRoles.add(userRole);
+    }
+
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", userName=" + userName + ", userForname=" + userForname + ", userEmail=" + userEmail
+                + ", bundleUserQuestions=" + bundleUserQuestions + "]";
+    }
+}
---
+++ devoxx-game/src/test/java/fr/soat/devoxx/game/services/impl/UserServiceTest.java
@@ -0,0 +1,48 @@
+package fr.soat.devoxx.game.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.soat.devoxx.game.model.DevoxxUser;
+import fr.soat.devoxx.game.model.UserRoles;
+import fr.soat.devoxx.game.services.UserServices;
+
+public class UserServiceTest extends GenericTestCase {
+
+	private static final String USER_EMAIL = "f.ostyn@gmail.com";
+	private static final String USER_NAME = "francois";
+	
+	@Autowired
+	UserServices userServices;
+	
+	@Test
+	public void testUserCreation() {
+		DevoxxUser user = new DevoxxUser();
+		user.setUserEmail(USER_EMAIL);
+		user.setUserName(USER_NAME);
+		user.setUserForname("ostyn");
+		List<UserRoles> userRoles = new ArrayList<UserRoles>();
+		userRoles.add(createUserRole("ROLE_ADMIN"));
+		userRoles.add(createUserRole("ROLE_FAKE"));
+		user.setUserRoles(userRoles);
+		userServices.createUser(user);
+	}
+
+	@Test
+	public void testUserSelect() {
+		DevoxxUser userByName = userServices.getUserByName(USER_NAME);
+		assertNotNull(userByName);
+		assertEquals(userByName.getUserEmail(), USER_EMAIL);
+		assertEquals(userByName.getUserRoles().size(), 2);
+	}
+	
+	private UserRoles createUserRole(String roleName) {
+		UserRoles roles = new UserRoles();
+		roles.setRoleName(roleName);
+		return roles;
+	}
+
+}
