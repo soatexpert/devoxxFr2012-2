ae883b6d48752d95dbf2eb942c19586df32101a7
aurelien <aure77+github@gmail.com>
2012-04-03 23:08:17 +0200

    Equals + HashCode

--- devoxx-game/src/main/java/fr/soat/devoxx/game/model/DevoxxUser.java
+++ devoxx-game/src/main/java/fr/soat/devoxx/game/model/DevoxxUser.java
@@ -165,17 +165,69 @@ public class DevoxxUser implements Serializable, UserDetails {
     public boolean isReglementAccepted() {
         return reglementAccepted;
     }
+    
+    @Transient
+    public String getMailHash() {
+        String n_email = Strings.isNullOrEmpty(userEmail) ? "" : userEmail.trim().toLowerCase();
+        return DigestUtils.md5Hex(n_email);
+    }   
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (commercialEmailAccepted ? 1231 : 1237);
+        result = prime * result + (enabled ? 1231 : 1237);
+        result = prime * result + (reglementAccepted ? 1231 : 1237);
+        result = prime * result + ((userEmail == null) ? 0 : userEmail.hashCode());
+        result = prime * result + ((userForname == null) ? 0 : userForname.hashCode());
+        result = prime * result + ((userRoles == null) ? 0 : userRoles.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DevoxxUser other = (DevoxxUser) obj;
+        if (commercialEmailAccepted != other.commercialEmailAccepted)
+            return false;
+        if (enabled != other.enabled)
+            return false;
+        if (reglementAccepted != other.reglementAccepted)
+            return false;
+        if (userEmail == null) {
+            if (other.userEmail != null)
+                return false;
+        } else if (!userEmail.equals(other.userEmail))
+            return false;
+        if (userForname == null) {
+            if (other.userForname != null)
+                return false;
+        } else if (!userForname.equals(other.userForname))
+            return false;
+        if (userRoles == null) {
+            if (other.userRoles != null)
+                return false;
+        } else if (!userRoles.equals(other.userRoles))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
 
     @Override
     public String toString() {
         return "DevoxxUser [userId=" + userId + ", username=" + username + ", userForname=" + userForname + ", userEmail=" + userEmail + ", reglementAccepted="
                 + reglementAccepted + ", commercialEmailAccepted=" + commercialEmailAccepted + ", nextEventsAccepted=" + nextEventsAccepted
                 + ", isAcceptedQrCode=" + isAcceptedQrCode + ", enabled=" + enabled + ", userRoles=" + userRoles + "]";
-    }
-
-    @Transient
-    public String getMailHash() {
-        String n_email = Strings.isNullOrEmpty(userEmail) ? "" : userEmail.trim().toLowerCase();
-        return DigestUtils.md5Hex(n_email);
     }
 }
