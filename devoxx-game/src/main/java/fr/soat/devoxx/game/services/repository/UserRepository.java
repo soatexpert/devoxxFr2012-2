4c01b04232b5a75a8f1249ea913002b0b682fcdf
aurelien <aure77+github@gmail.com>
2012-04-05 22:55:58 +0200

    Correction requete ranking

--- devoxx-game/src/main/java/fr/soat/devoxx/game/services/repository/UserRepository.java
+++ devoxx-game/src/main/java/fr/soat/devoxx/game/services/repository/UserRepository.java
@@ -39,7 +39,7 @@ public interface UserRepository extends CrudRepository<DevoxxUser, Long> {
     @Query("FROM DevoxxUser u where u.username=?1")
     public DevoxxUser findUserByName(String username);
 
-    @Query("FROM DevoxxUser u where u.enabled = true order by u.userScores[?1].score DESC, u.userScores[?1].totalTime DESC, u.userForname ASC limit 10")
+    @Query("FROM DevoxxUser u where u.enabled = true order by u.userScores[?1].score DESC, u.userScores[?1].totalTime ASC, u.userForname ASC limit 10")
     Page<DevoxxUser> findTopTen(QuestionPackType questionPack, Pageable page);
 
     @Query("FROM DevoxxUser u where u.enabled = true and ( (u.userScores[?3].score > ?1) or (u.userScores[?3].score = ?2 and u.userScores[?3].totalTime < ?2))")
